914a708654ec150bb739e842e4cfd2dbfa85bf39
Adam Lacue <adam.lacue@gmail.com>
2024-01-17 09:58:48 -0500

    Initial Commit

---
+++ Stanford/StanfordApp.swift
@@ -0,0 +1,17 @@
+//
+//  StanfordApp.swift
+//  Stanford
+//
+//  Created by Adam2 on 1/17/24.
+//
+
+import SwiftUI
+
+@main
+struct StanfordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
